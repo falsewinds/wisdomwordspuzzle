b59c26a73bbc66420368fa0ec5ff9909a0f8db86
kj <falsewinds@gmail.com>
2023-11-18 17:46:34 +0800

    upgrade.

---
+++ js/wisdomwords.js
@@ -0,0 +1,33 @@
+window.wwdb = [
+    {
+        "sentence": "存在即合理",
+        "grouping": [ 2, 1, 2 ],
+        "separate": "by_group",
+        "filled": [ false, true, false ]
+    },
+    {
+        "sentence": "存在即合理",
+        "grouping": [ 2, 1, 2 ],
+        "separate": "by_group",
+    },
+    {
+        "sentence": "存在即合理",
+        "grouping": [ 2, 1, 2 ],
+        "filled": [ false, false, true, false, false ]
+    },
+    {
+        "sentence": "存在即合理",
+        "grouping": [ 2, 1, 2 ]
+    },
+    {
+        "sentence": "我思故我在",
+        "grouping": [ 2, 1, 2 ],
+        "separate": "by_group"
+    },
+    {
+        "sentence": ["沒有經過反省的生命","是不值得活的"],
+        "grouping": [ 2, 2, 2, 1, 2, 1, 1, 2, 1, 1 ],
+        "separate": "by_group",
+        "filled": [ false, false, false, true, false, false, false, false, false, true ]
+    }
+];
